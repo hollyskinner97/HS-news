086276152d752b76b23fe120afe0201f3452a28b
Holly Skinner <hollyskinner97@gmail.com>
2025-01-27 14:23:09 +0000

    add task 4 GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const db = require("../db/connection");
 const testData = require("../db/data/test-data/index");
+const articles = require("../db/data/test-data/articles");
 
 beforeEach(() => {
   return seed(testData);
@@ -56,6 +57,46 @@ describe("GET /api/topics", () => {
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("Route not found");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: should return an article object with the correct properties, given a valid article id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const article = body.article;
+
+        expect(article).toMatchObject({
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: expect.any(Number),
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+        });
+      });
+  });
+
+  test("should return 404 when given an article id which is out of range / invalid", () => {
+    return request(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.error).toBe("Not found");
+      });
+  });
+
+  test("should return 400 when given an article id which is not a number", () => {
+    return request(app)
+      .get("/api/articles/whoops")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.error).toBe("Bad request");
       });
   });
 });
