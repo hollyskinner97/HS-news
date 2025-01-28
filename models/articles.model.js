b65e71f748b9fef4cfaf619178fed6e140ae7d55
Holly Skinner <hollyskinner97@gmail.com>
2025-01-28 16:53:25 +0000

    add task 12 adding topic query to GET articles

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const { checkTopicExists } = require("./utils");
 
 exports.selectArticleById = (article_id) => {
   return db
@@ -12,13 +13,14 @@ exports.selectArticleById = (article_id) => {
     });
 };
 
-exports.selectArticles = (sort_by = "created_at", order = "desc") => {
+exports.selectArticles = (sort_by = "created_at", order = "desc", topic) => {
   let SQLString = `
   SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id):: INT AS comment_count 
   FROM articles
   LEFT JOIN comments ON comments.article_id = articles.article_id
-  GROUP BY articles.article_id`;
+  `;
 
+  const queryValues = [];
   const validSortBy = ["title", "topic", "author", "created_at", "votes"];
   const validOrder = ["asc", "desc", "ASC", "DESC"];
 
@@ -27,12 +29,20 @@ exports.selectArticles = (sort_by = "created_at", order = "desc") => {
   }
   if (!validOrder.includes(order)) {
     throw { status: 400, msg: "Invalid order" };
-  } else {
-    SQLString += ` ORDER BY articles.${sort_by} ${order.toUpperCase()}`;
   }
 
-  return db.query(SQLString).then(({ rows }) => {
-    return rows;
+  const checkTopic = topic
+    ? checkTopicExists(topic).then(() => {
+        SQLString += ` WHERE articles.topic = $1`;
+        queryValues.push(topic);
+      })
+    : Promise.resolve();
+
+  return checkTopic.then(() => {
+    SQLString += ` GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order.toUpperCase()}`;
+    return db.query(SQLString, queryValues).then(({ rows }) => {
+      return rows;
+    });
   });
 };
 
